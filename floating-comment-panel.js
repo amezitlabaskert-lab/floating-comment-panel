edc4493465a44c22fec3c6719ee3f41fac6b51cf
amezitlabaskert <amezitlabaskert@gmail.com>
2026-06-23 05:58:47 +0200

    Update FCP version and avatar image source

--- floating-comment-panel.js
+++ floating-comment-panel.js
@@ -261,7 +261,7 @@
 function createFloatingCommentPanel() {
     if (document.getElementById('floating-comment-drawer')) return;
 
-    var FCP_VERSION = '6.2';
+    var FCP_VERSION = '6.2.3';
 
     var drawer = document.createElement('div');
     drawer.id = 'floating-comment-drawer';
@@ -540,7 +540,7 @@ function magyaritEchoThread() {
         if (el.dataset.fcpGuestDone) return;
         el.dataset.fcpGuestDone = '1';
         var img = document.createElement('img');
-        img.src = 'https://i.imgur.com/GUBj1O3.png';
+        img.src = 'https://i.imgur.com/8DJZiU3.png';
         img.alt = '';
         img.setAttribute('aria-hidden', 'true');
         img.style.cssText = el.style.cssText;
